0d6db36bae17b2cce7744dac64a4a7e9b901eb2b
yazooon <100956980+yazooon@users.noreply.github.com>
2022-03-08 13:49:26 +0200

    adding new person detales for testing

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,7 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
-		
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(27);
+		p.setFirstName("yazan");
+		p.setLastName("dayoub");
+		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
